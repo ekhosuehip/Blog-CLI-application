d163e1b518ce6e151425cd49a05e03d4688dd023
ekhosuehip <ekhosuehipromise8@gmail.com>
2025-01-11 11:53:04 +0000

    Get the current directory

--- bin/index.js
+++ bin/index.js
@@ -6,3 +6,12 @@ import fs from 'fs';
 import path from 'path';
 import inquirer from 'inquirer';
 
+// Get the current directory
+const __dirname = path.dirname(new URL(import.meta.url).pathname);
+
+const blogDir = path.resolve(__dirname, 'blogs');
+
+// Ensure the blogs directory exists
+if (!fs.existsSync(blogDir)) {
+  fs.mkdirSync(blogDir);
+}
